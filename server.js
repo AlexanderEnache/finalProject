430a4548e33e7b9c831095051116714f67ee67c5
Alex Enache <ragnarxon@gmail.com>
2019-03-08 08:30:48 -0500

    fixed connection 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const route = require("route.js");
+const route = require("./route.js");
 
 app.use(express.static("./public"));
 app.use(express.json());
