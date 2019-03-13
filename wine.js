62f1001070c2d42166e2c02b10ad8cb5a0e3d148
Julia Fowler <juliafowler@live.com>
2019-03-13 16:46:01 -0400

    set timeout function

--- wine.js
+++ wine.js
@@ -19,4 +19,15 @@ wine.get("/mainsearch/:searchterm", function (req, res) {
   });
 });
 
+// wine.put("/wine/:priceupdate", function(req, res){
+//   pool.query("update wiskey set description='fuckin eh right??' where id=")
+// })
+wine.get("/winesearch/:id", function (req, res) {
+  console.log(req.params.id + "fgh")
+  pool.query(`select description from holdall where id=${req.params.id}`).then(function(result) {
+	console.log(result);
+    res.send(result.rows);
+  });
+});
+
 module.exports = wine;
