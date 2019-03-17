b147c48db8c83798e60702e3aae002dc3c5e9d99
Tywin <certainvictorythp@thp@gmail.com>
2019-03-17 15:06:46 -0400

    toast and added some stylo back

--- public/app.main.js
+++ public/app.main.js
@@ -1,98 +1,104 @@
 "use strict";
 
 const main = {
-	
+
 	templateUrl: "main.html",
-	controller:["Serv", "$location", "$scope", function(Serv, $location, $scope){
-		
+	controller: ["Serv", "$location", "$scope", function (Serv, $location, $scope) {
+
 		const vm = this;
 		vm.show = false;
 		vm.wine = [];
 		vm.showArray = [];
 
-		Serv.getWine(10).then(function(res){
+		Serv.getWine(10).then(function (res) {
 			vm.wine = res.data;
-			for(let i = 0; i < vm.wine.length; i++){
+			for (let i = 0; i < vm.wine.length; i++) {
 				vm.showArray.push(false);
 			}
 			// console.log(res);
 		});
 		vm.ooo = 10;
-		vm.load = function(){
-			
+		vm.load = function () {
+
 			vm.ooo = vm.ooo + 5;
-			Serv.getWine(vm.ooo).then(function(res){
+			Serv.getWine(vm.ooo).then(function (res) {
 				vm.wine = res.data;
-				for(let i = 0; i < vm.wine.length; i++){
+				for (let i = 0; i < vm.wine.length; i++) {
 					vm.showArray.push(false);
 				}
 				// console.log(res);
 			});
 		}
 
-		vm.select = function(bottle, index){
-			
-			for(let i = 0; i < vm.showArray.length; i++){
+		vm.select = function (bottle, index) {
+
+			for (let i = 0; i < vm.showArray.length; i++) {
 				vm.showArray[i] = false;
 			}
 
 			vm.showArray[index] = true;
 
-			if(bottle){
+			if (bottle) {
 				vm.show = true;
 				vm.bottle = bottle;
-			}else{
+			} else {
 				vm.show = false;
 			}
-			
+
 		}
-		
-		vm.search = function(keyword) {
+
+		vm.search = function (keyword) {
 			console.log(keyword)
 			Serv.search(keyword);
 			//$location.path("/searchresults");
 		};
 		vm.wallet = Serv.getCredits()
 
-		vm.buy = function(id, quantity) {
+		vm.buy = function (id, quantity) {
+			vm.quantity = quantity;
 			Serv.buy(id, quantity);
-			vm.wallet = Serv.getCredits()
+			vm.wallet = Serv.getCredits();
+			launch_toast()
 		};
 
 
-		
-		
-		
 
-  /* $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  }; */
-  //$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
- /*  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
-      ]
-    }
-  }; */
-		
-		
-		
-		
-		
+			function launch_toast() {
+				const x = document.getElementById("toast")
+				x.className = "show";
+				setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+		};
+
+
+		/* $scope.onClick = function (points, evt) {
+			console.log(points, evt);
+		}; */
+		//$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+		/*  $scope.options = {
+			 scales: {
+				 yAxes: [
+					 {
+						 id: 'y-axis-1',
+						 type: 'linear',
+						 display: true,
+						 position: 'left'
+					 },
+					 {
+						 id: 'y-axis-2',
+						 type: 'linear',
+						 display: true,
+						 position: 'right'
+					 }
+				 ]
+			 }
+		 }; */
+
+
+
+
+
 	}]
-	
+
 }
 
 angular.module("WW").component("main", main);
